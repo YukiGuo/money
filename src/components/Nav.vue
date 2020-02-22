3cb24ceaf891969fb09385d2b50e6a74d98f5778
Guo <2540865175@qq.com>
2020-02-22 21:17:18 +0800

    ââIcSvg配置

---
+++ src/components/Nav.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="nav">
+        <hr/>
+        <router-link to="/money"> 记账</router-link>
+        <router-link to="/statistics"> 统计</router-link>
+        <router-link to="/label">
+        <svg>
+                <use xlink:href="#money"/>
+        </svg>
+            标签
+        </router-link>
+    </div>
+
+
+</template>
+
+<script lang='ts'>
+    const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+    try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+    export default {
+        name: 'Nav'
+    };
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Statistics from '@/views/Statistics.vue';
+import Label from '@/views/Label.vue';
+import Money from '@/views/Money.vue';
+import Notfound from '@/views/Notfound.vue';
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+   {
+        path: '/',
+        redirect: '/money'
+    },
+    {
+        path:'/money',
+        component: Money
+    },
+    {
+        path:'/statistics',
+        component: Statistics
+    },
+    {
+        path:'/label',
+        component: Label
+    },
+    {
+        path:'*',
+        component: Notfound
+    },
+
+
+];
+
 
 const router = new VueRouter({
-  routes
-})
+    routes
+});
 
-export default router
+export default router;
