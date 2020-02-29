5e96dd98de819d37b621f57947375118e8e6b318
Guo <2540865175@qq.com>
2020-02-29 16:32:06 +0800

    NumberPad组件功能实现

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,34 +1,77 @@
 <template>
     <div class="numberPad">
-        <div class="output">100</div>
+        <div class="output">{{output}}</div>
         <div class="buttons">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>删除</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>清空</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button class="ok">OK</button>
-            <button class="zero">0</button>
-            <button>.</button>
+            <button @click="inputContent">1</button>
+            <button @click="inputContent">2</button>
+            <button @click="inputContent">3</button>
+            <button @click="remove">删除</button>
+            <button @click="inputContent">4</button>
+            <button @click="inputContent">5</button>
+            <button @click="inputContent">6</button>
+            <button @click="clear">清空</button>
+            <button @click="inputContent">7</button>
+            <button @click="inputContent">8</button>
+            <button @click="inputContent">9</button>
+            <button @click="ok" class="ok">OK</button>
+            <button @click="inputContent" class="zero">0</button>
+            <button @click="inputContent">.</button>
         </div>
     </div>
 </template>
 
 <script lang='ts'>
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
 
-    export default {
-        name: 'NumberPad'
-    };
+    @Component
+    export default class NumberPad extends Vue {
+        output = '0';
+
+        clear() {
+            this.output = '0';
+        }
+
+        remove() {
+            if (this.output.length === 1) {
+                this.output = '0';
+            } else {
+                this.output = this.output.slice(0, -1);
+            }
+        }
+
+        ok() {
+            return;
+        }
+
+        inputContent(event: MouseEvent) {
+            //强制指定类型
+            const button = (event.target as HTMLButtonElement);
+            const input = button.textContent!; //！表示这个值不会为空
+            if (this.output.length === 16) {
+                return;
+            }
+            if (this.output === '0') {
+                if ('0123456789'.indexOf(input) >= 0) {
+                    this.output = input;
+                } else if (input === '.') {
+                    this.output += input;
+                }
+                return
+            }
+            if (this.output.indexOf('.') >= 0 && input === '.') {
+                return;
+            }
+
+            this.output += input;
+
+        }
+    }
 </script>
 
 <style lang='scss' scoped>
     @import '~@/assets/style/variable.scss';
+
     .numberPad {
         > .output {
             @extend %innerShadow;
@@ -41,6 +84,7 @@
 
         > .buttons {
             @extend %x;
+
             > button {
                 float: left;
                 width: 25%;
