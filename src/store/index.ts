c625e16a258549a0f5082fb7e4213ca9975c4fcf
Guo <2540865175@qq.com>
2020-03-09 22:47:14 +0800

    edict-vuexââ

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,14 +2,22 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import clone from '@/lib/clone';
 import createId from '@/lib/createId';
+import router from '@/router';
 
 Vue.use(Vuex);
 
+
+type RootState ={
+    recordList: RecordItem[];
+    tagList: Tag[];
+    currentTag?: Tag;
+}
  const store =new Vuex.Store({
   state: {
-      recordList:[] as RecordItem[],
-      tagList: [] as Tag[],
-  },
+      recordList:[],
+      tagList: [],
+      currentTag:undefined
+  }  as RootState,
   mutations: {
     fetchRecords(state) {
       state.recordList=JSON.parse(window.localStorage.getItem('recordList') || '[]')as RecordItem[];
@@ -26,13 +34,34 @@ Vue.use(Vuex);
       fetchTags(state){
           state.tagList= JSON.parse( window.localStorage.getItem('tagList')||'[]');
       },
+      getCurrentTag(state,id: string){
+              state.currentTag= state.tagList.filter(item=>(item.id===id))[0];
+      },
       saveTags(state){
           window.localStorage.setItem('tagList',JSON.stringify(state.tagList))
       },
-      removeTag(state,tag: Tag){
-          const index= state.tagList.indexOf(tag);
-          state.tagList.splice(index,1);
-          store.commit('saveTag');
+      removeTag(state,id){
+          // const index= state.tagList.indexOf(tag);
+          // state.tagList.splice(index,1);
+          // store.commit('saveTags');
+
+
+          let index = -1;
+          for (let i = 0; i < state.tagList.length; i++) {
+              if (state.tagList[i].id === id) {
+                  index = i;
+                  break;
+              }
+          }
+          if (index >= 0) {
+              state.tagList.splice(index, 1);
+              store.commit('saveTags');
+              alert("删除成功");
+              router.back();
+          } else {
+              window.alert('删除失败');
+          }
+
       },
       createTag(state,name: string){
           const names=state.tagList.map(item=>item.name);
@@ -45,12 +74,13 @@ Vue.use(Vuex);
               alert("添加成功");
           }
       },
-      updateTag(state,id: string){
+      updateTag(state,payload: { id: string; name: string }){
+        const {id,name} =payload;
           const idList=state.tagList.map((item)=> item.id);
           if(idList.indexOf(id)>=0){
               const nameList=state.tagList.map( item => item.name);
               if(nameList.indexOf(name)>=0){
-                  console.log('重复');
+                  window.alert('标签名重复了')
               }else{
                   console.log('成功');
                   const tag =state.tagList.filter((item)=>item.id===id)[0];
--- src/views/LabelEdict.vue
+++ src/views/LabelEdict.vue
@@ -25,31 +25,30 @@
     import {Component} from 'vue-property-decorator';
     import FormItem from '@/components/Money/FormItem.vue';
     import Button from '@/components/Button.vue';
-    import store from '@/store/index2';
     @Component({
         components: {Button, FormItem}
     })
     export default class LabelEdict extends Vue{
-        tag?: { id: string; name: string };
+        get tag() {
+            return this.$store.state.currentTag;
+        }
         created(){
+            this.$store.commit('fetchTags');
            const id= this.$route.params.id;
-            const tag=store.findTag(id);
-            if(tag){
-              this.tag=tag;
-            }else{
+            this.$store.commit('getCurrentTag',id);
+            if(!this.tag){
                 this.$router.replace('/404');
             }
         }
         update(name: string){
+            console.log(name);
             if(this.tag) {
-                store.updateTag(this.tag.id, name);
+                this.$store.commit('updateTag',{ id: this.tag.id,name:name});
             }
         }
         remove(){
             if(this.tag) {
-                store.removeTag(this.tag);
-               alert("删除成功");
-                this.$router.replace('/label');
+                this.$store.commit('removeTag',this.tag.id);
             }
         }
     }
