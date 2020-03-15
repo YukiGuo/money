bf676881eb31df66b47b59f131e2bdffc700785f
Guo <2540865175@qq.com>
2020-03-15 13:10:50 +0800

    â封装tab组件

---
+++ src/components/tab.vue
@@ -0,0 +1,69 @@
+<template>
+    <ul class="tab">
+        <li
+                v-for="item in list" :key="item.value"
+                :class="isClass(item)"
+                @click="select(item)"
+        >
+            {{item.text}}
+
+        </li>
+    </ul>
+</template>
+
+<script lang='ts'>
+    import Vue from 'vue'
+    import {Component,Prop} from 'vue-property-decorator';
+    type dataSource={
+        value: string; text: string;
+    }
+    @Component
+    export default class Tab extends Vue{
+        @Prop({required:true}) list!: [];
+        @Prop(String) readonly value!: string;
+        @Prop(String) readonly classPrefix!: string;
+         isClass(item: dataSource){
+             return {
+                 selected:this.value=== item.value,
+                 [this.classPrefix + '-tab-item']:this.classPrefix
+             }
+         }
+        select(item: dataSource) {
+          const  type=item.value;
+            if (type !== '-' && type !== '+') {
+                console.log('错误')
+            }
+            this.$emit('update:value',item.value);
+        }
+    }
+</script>
+
+<style lang='scss' scoped>
+    @import '~@/assets/style/variable.scss';
+
+    .tab {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+         li {
+            position: relative;
+            height: 64px;
+            width: 50vw;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background: #bbb;
+            font-size: 24px;
+            &.selected::after {
+                content: '';
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                width: 100%;
+                height: 4px;
+                background: #8a8a8a;
+            }
+        }
+    }
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,16 +1,35 @@
 <template>
-    <div> <Layout>
-        statistics
-    </Layout>
+    <div>
+        <Layout>
+            <Tab :list="arr" :value.sync="type" class-prefix="type"/>
+            <Tab :list ="arr1" :value.sync="interval" class-prefix="interval"/>
+       </Layout>
     </div>
 </template>
 
 <script lang='ts'>
-    export default {
-        name: 'Statistics'
-    };
+    import Vue from 'vue'
+    import {Component} from 'vue-property-decorator';
+    import Tab from '@/components/Tab.vue';
+    @Component({
+        components: {Tab}
+    })
+    export default class Statistics extends Vue{
+        type='-';
+        interval='day';
+        arr=[{value: '-',text: "支出"},{value:'+',text:"收入"}];
+        arr1=[{value:'day',text:"按天"},{value: 'week',text: "按周"},{value: "month",text:"按月"}]
+    }
 </script>
-
 <style lang='scss' scoped>
+    ::v-deep .type-tab-item {
+        background: white;
+        &.selected {
+            background-color: #c4c4c4;
+            &::after{
+               display: none;
+            }
+        }
+    }
 
 </style>
