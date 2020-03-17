ddde88ff34b3769780b2338b50d7e6e0517dd5e8
Guo <2540865175@qq.com>
2020-03-17 21:44:02 +0800

    â完成统计页面ï,clone页js的输入输出类型

--- src/lib/clone.ts
+++ src/lib/clone.ts
@@ -1,4 +1,4 @@
-function clone(data: any) {
+function clone<T>(data: T): T {// 更新clone返回值的类型和输入值得类型一致
         return JSON.parse(JSON.stringify(data));
 }
 export default clone;
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,12 +4,15 @@
             <Tab :list="typeList" :value.sync="type" class-prefix="type"/>
             <Tab :list="intervalList" :value.sync="interval" class-prefix="interval"/>
             <ul class="record">
-                <li v-for="(group,index) in result" :key="index">
-                    <h3 class="title"> {{ beautify(group.title)}}</h3>
+                <li v-for="(group,index) in list" :key="index">
+                    <div class="title">
+                    <span> {{ beautify(group.title)}}</span>
+                        <span>￥{{group.total}}</span>
+                    </div>
                     <ol>
                         <li v-for="(item,index) in group.items" :key="index">
                             <span class="tag">{{item.tags[0].name}}</span>
-                            <span class="notes">无{{item.notes}}</span>
+                            <span class="notes">{{item.notes}}</span>
                             <span class="money">￥{{item.amount}}</span>
                         </li>
                     </ol>
@@ -39,12 +42,15 @@
         > li {
 
             > .title {
-                   line-height: 40px;
-                   padding: 0 10px;
+                display: flex;
+                justify-content: space-between;
+                line-height: 40px;
+                padding: 0 10px;
             }
-            >ol{
-                >li{
-            background: white;
+
+            > ol {
+                > li {
+                    background: white;
                     min-height: 35px;
                     margin-bottom: 2px;
                     display: flex;
@@ -52,8 +58,9 @@
                     justify-content: space-between;
                     align-items: center;
                     padding: 0 10px;
-                    >.notes{
-                      margin-right: auto;
+
+                    > .notes {
+                        margin-right: auto;
                         margin-left: 8px;
                     }
                 }
@@ -68,6 +75,7 @@
     import typeList from '@/constants/typelist';
     import intervalList from '@/constants/intervalList';
     import dayjs from 'dayjs';
+    import clone from '@/lib/clone';
 
     @Component({
         components: {Tab}
@@ -81,50 +89,73 @@
         get recordList() {
             return (this.$store.state as RootState).recordList;
         }
-
-        get result() {
+        get list() {
             const {recordList} = this;
-            type hashTableValue = {
-                title: string; items: RecordItem[][];
+            if (recordList.length === 0) {
+                return [] as GroupList;
             }
-            const hashTable: { [key: string]: hashTableValue } = {};
-            for (let i = 0; i < recordList.length; i++) {
-                const [date, time] = recordList[i].createdDate!.split('T');
-                hashTable[date] = hashTable[date] || {title: date, items: []};
-                hashTable[date].items.push(recordList[i]);
+            const newList = clone(recordList)
+                .filter(item=>item.type===this.type)
+                .sort((a, b) => dayjs(b.createdDate).valueOf() - dayjs(a.createdDate).valueOf());
+            if(newList.length===0){
+                return [] as GroupList;
             }
-            return hashTable;
+            type GroupList = { title: string; items: RecordItem[]; total?: number }[];
+            const groupList: GroupList = [{
+                title: dayjs(newList[0].createdDate).format('YYYY-MM-DD'),
+                items: [newList[0]]
+            }];
+
+            for (let i = 1; i < newList.length; i++) {
+                const current = newList[i];
+                const last = groupList[groupList.length - 1];
+                if (dayjs(current.createdDate).isSame(dayjs(last.title), 'day')) {
+                    last.items.push(current);
+                } else {
+                    groupList.push({title: dayjs(current.createdDate).format('YYYY-MM-DD'), items: [current]});
+                }
+            }
+            groupList.map(group => {
+                group.total = group.items.reduce((sum, item) => {
+                    return (sum + item.amount);
+                },0);
+            });
+            return groupList;
+
         }
-      beautify(string: string){
-          // const d =new Date(Date.parse(string));
-          // const y= d.getFullYear();
-          // const  m =d.getMonth();
-          // const dd =d.getDay();
-          const day=dayjs(string);
-          const today=new Date();
-          const oneday =86400*1000;
-          // if(y===today.getFullYear() && m===today.getMonth() && dd===today.getDay())
-          if(day.isSame(today,'day'))//day简化
-          {return "今天"}else if(
-            day.isSame(today.valueOf()-oneday,'day')
-          ){
-              return "昨天"
-          }else if(
-            day.isSame(dayjs().subtract(2,'day'),'day')
-          ){
-              return "前天"
-          }else{
-              if(day.isSame(today,'year')) {
-                  return day.format("M月D日")
-              }else{
-                  return day.format("YYYY年M月D日")
-              }
-          }
-      }
+
+        beautify(string: string) {
+            // const d =new Date(Date.parse(string));
+            // const y= d.getFullYear();
+            // const  m =d.getMonth();
+            // const dd =d.getDay();
+            const day = dayjs(string);
+            const today = new Date();
+            const oneday = 86400 * 1000;
+            // if(y===today.getFullYear() && m===today.getMonth() && dd===today.getDay())
+            if (day.isSame(today, 'day'))//day简化
+            {
+                return '今天';
+            } else if (
+                day.isSame(today.valueOf() - oneday, 'day')
+            ) {
+                return '昨天';
+            } else if (
+                day.isSame(dayjs().subtract(2, 'day'), 'day')
+            ) {
+                return '前天';
+            } else {
+                if (day.isSame(today, 'year')) {
+                    return day.format('M月D日');
+                } else {
+                    return day.format('YYYY年M月D日');
+                }
+            }
+        }
+
         mounted() {
             this.$store.commit('fetchRecords');
-            const api= dayjs();
-            console.log(api);
+            const api = dayjs();
         }
 
     }
