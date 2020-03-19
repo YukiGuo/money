3d3a799e55bfe16d6b89b3a228a86a0a9e0ac114
Guo <2540865175@qq.com>
2020-03-19 20:13:28 +0800

    导入了标签的Logo

---
+++ src/constants/tagList.ts
@@ -0,0 +1,26 @@
+type Tag = {
+    id: number;
+    name: string;
+    icon: string;
+    type: string;
+}
+
+const tagArray: Tag[] = [
+    {id: 1, name: '餐饮', icon: '#canyin', type: '-'},
+    {id: 2, name: '购物', icon: 'gouwuche', type: '-'},
+    {id: 3, name: '住房', icon: 'zhufang', type: '-'},
+    {id: 4, name: '交通', icon: 'jiaotong', type: '-'},
+    {id: 5, name: '娱乐', icon: 'yule', type: '-'},
+    {id: 6, name: '文教', icon: 'wenjiaoban', type: '-'},
+    {id: 7, name: '通讯', icon: 'tongxun', type: '-'},
+    {id: 8, name: '旅行', icon: 'lvxing', type: '-'},
+    {id: 9, name: '医疗', icon: 'yiliao', type: '-'},
+    {id: 10, name: '购物', icon: 'gouwuche', type: '-'},
+    {id: 11, name: '人情', icon: 'renqing', type: '-'},
+    {id: 12, name: '薪资', icon: 'xinzi', type: '+'},
+    {id: 13, name: '奖金', icon: 'jiangjin', type: '+'},
+    {id: 14, name: '红包', icon: 'hong', type: '+'},
+    {id: 15, name: '其它', icon: 'qita', type: '-'},
+    {id: 16, name: '其它收入', icon: 'qitashouru', type: '+'},
+];
+export default tagArray;
